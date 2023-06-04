cf9f06cd24049af8980641b757b17b39aa2f0252
ryomac <rats.galf@gmail.com>
2023-06-04 07:05:54 +0000

    make a solv.py

---
+++ src/solve.py
@@ -0,0 +1,11 @@
+import io
+import sys
+from pprint import pprint
+from typing import List, Tuple, Dict, Set, Optional, Union, Any, Callable
+
+_INPUT = """\
+
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+#
